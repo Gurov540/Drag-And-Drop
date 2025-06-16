df95071ffb4c003e5dc5c9e3874fffa5086ef117
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-06-16 19:44:50 +0300

    input backlight is removed at startup

--- js/main.js
+++ js/main.js
@@ -7,15 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const doneItems = document.getElementById("done-items");
   const columns = document.querySelectorAll(".items");
 
-  // Подсветка поля ввода при загрузке
-  setTimeout(() => {
-    taskInput.focus();
-    taskInput.classList.add("task-input-highlight");
-    setTimeout(() => {
-      taskInput.classList.remove("task-input-highlight");
-    }, 1500);
-  }, 500);
-
   //обновление счтчиков задач
   function updateTaskCounts() {
     document.querySelectorAll(".column").forEach((column) => {
